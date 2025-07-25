f010f13bfa71ff57e8fec4790f2664835fea94f3
khadeeja-zakkiya <zakkiyakhadeeja@gmail.com>
2025-07-25 04:21:07 -0700

    more logo added

--- src/components/Channel.jsx
+++ src/components/Channel.jsx
@@ -19,29 +19,29 @@ import logo14 from '../assets/logo14.jpg';
 import logo15 from '../assets/logo15.png';
 import logo16 from '../assets/logo16.png';
 import logo17 from '../assets/logo17.png';
-// import logo18 from '../assets/logo18.png';
-// import logo19 from '../assets/logo19.png';
-// import logo20 from '../assets/logo20.png';
+import logo18 from '../assets/logo18.jpg';
+import logo19 from '../assets/logo19.jpg';
+import logo20 from '../assets/logo20.png';
 
 // Paid channel logos
-// import paid1 from '../assets/paid1.png';
-// import paid2 from '../assets/paid2.png';
-// import paid3 from '../assets/paid3.png';
-// import paid4 from '../assets/paid4.png';
-// import paid5 from '../assets/paid5.png';
-// import paid6 from '../assets/paid6.png';
-// import paid7 from '../assets/paid7.png';
-// import paid8 from '../assets/paid8.png';
+import paid1 from '../assets/paid1.png';
+import paid2 from '../assets/paid2.png';
+import paid3 from '../assets/paid3.png';
+import paid4 from '../assets/paid4.jpg';
+import paid5 from '../assets/paid5.png';
+import paid6 from '../assets/paid6.png';
+import paid7 from '../assets/paid7.png';
+import paid8 from '../assets/paid8.jpg';
 
 const Channel = () => {
   const demoLogos = [
     logo1,logo2,logo3,logo4,logo5,logo6,logo7,logo8,logo9,logo10,logo11,logo12,logo13,logo14,logo15,
-    logo16,logo17
+    logo16,logo17,logo18,logo19,logo20
   ];
 
   const paidLogos = [
-    // paid1, paid2, paid3, paid4, paid5,
-    // paid6, paid7, paid8,
+    paid1,paid2,paid3,paid4,paid5,paid6,paid7,paid8
+   
   ];
 
   return (
@@ -57,6 +57,7 @@ const Channel = () => {
                 </div>
               ))}
             </div>
+            <br />
             <div className="buttons">
               <button className="deal-btn">Deal Now</button>
               <button className="try-btn">Try Free</button>
@@ -64,7 +65,7 @@ const Channel = () => {
           </div>
 
           <div className="bottom-section">
-            <h2>TOP PAID CHANNEL</h2>
+            <h1>TOP PAID CHANNEL</h1>
             <div className="logo-grid small-grid">
               {paidLogos.map((logo, index) => (
                 <div key={index} className="logo-circle small">
