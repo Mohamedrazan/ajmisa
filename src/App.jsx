e8f6756b8c7c467008c288ffcc04592acee6f41c
khadeeja-zakkiya <zakkiyakhadeeja@gmail.com>
2025-07-25 04:00:35 -0700

    channel add

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Iptvsection from './components/Iptvsection'
+import Channel from './components/Channel'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <Navbar/>
         <br /><br /><br />
         <Iptvsection/>
+        <Channel/>
     </div>
   )
 }
