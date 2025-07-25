53d521ea205ba2ea028094c5f93ff92c8ca556e0
khadeeja-zakkiya <zakkiyakhadeeja@gmail.com>
2025-07-25 04:08:20 -0700

    logo added

--- src/components/Channel.jsx
+++ src/components/Channel.jsx
@@ -12,13 +12,13 @@ import logo7 from '../assets/logo7.png';
 import logo8 from '../assets/logo8.png';
 import logo9 from '../assets/logo9.png';
 import logo10 from '../assets/logo10.png';
-// import logo11 from '../assets/logo11.png';
-// import logo12 from '../assets/logo12.png';
-// import logo13 from '../assets/logo13.png';
-// import logo14 from '../assets/logo14.png';
-// import logo15 from '../assets/logo15.png';
-// import logo16 from '../assets/logo16.png';
-// import logo17 from '../assets/logo17.png';
+import logo11 from '../assets/logo11.png';
+import logo12 from '../assets/logo12.jpg';
+import logo13 from '../assets/logo13.jpg';
+import logo14 from '../assets/logo14.jpg';
+import logo15 from '../assets/logo15.png';
+import logo16 from '../assets/logo16.png';
+import logo17 from '../assets/logo17.png';
 // import logo18 from '../assets/logo18.png';
 // import logo19 from '../assets/logo19.png';
 // import logo20 from '../assets/logo20.png';
@@ -35,7 +35,8 @@ import logo10 from '../assets/logo10.png';
 
 const Channel = () => {
   const demoLogos = [
-    logo1,logo2,logo3,logo4,logo5,logo6,logo7,logo8,logo9,logo10,
+    logo1,logo2,logo3,logo4,logo5,logo6,logo7,logo8,logo9,logo10,logo11,logo12,logo13,logo14,logo15,
+    logo16,logo17
   ];
 
   const paidLogos = [
